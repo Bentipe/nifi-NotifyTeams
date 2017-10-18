fe9e2d631a4e4574f2685192b7ba2f9910629a45
VE\mirodriguez <miguelnicolas.rodriguez@veinteractive.com>
2017-10-18 11:24:48 +0200

    added the first version of the processor with its all dependencies

---
+++ nifi-nifi_notify_teams-processors/src/main/java/teams/processors/nifi_notify_teams/Message.java
@@ -0,0 +1,29 @@
+package teams.processors.nifi_notify_teams;
+
+public class Message {
+
+    private String title;
+    private String text;
+
+    public Message(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
